955b7f9bd91cea4cfa68236f472a7da4f5f83018
RaphielSh <raphielsh@gmail.com>
2024-08-24 18:48:00 +0300

    Check of unexpected infinite loop

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include<iostream>
 #include<stdexcept>
+#include<unordered_set>
+
+void print(const auto& set){
+	for(const auto& elem : set)std::cout<<elem<<' ';
+	std::cout<<std::endl;
+}
+
 bool checkConjecture(long long unsigned int num){
 	if(num <= 0)throw std::invalid_argument("invalid argument");
-	long long unsigned int tmp = num;
+	std::unordered_set<long long unsigned int> lst;
 	while(1!=num){
 		num = (num%2) ? (3*num)+1 : num/2;
-		if(tmp == num)return false;
+		if(lst.contains(num))return false;
+		lst.insert(num);
     	}
 	return true;
 }
 
 void checkConjectureVerbose(long long unsigned int num){
 	if(num <= 0)throw std::invalid_argument("invalid argument");
-	long long unsigned int tmp = num;
+	std::unordered_set<long long unsigned int> lst;
 	while(1!=num){
 		num=(num%2)?(3*num)+1 : num/2;
-		if(tmp == num)std::cout<<"Check failed!"<<std::endl;
+		if(lst.contains(num))std::cout<<"Infinite loop with number "<<num<<std::endl;
 	}
 	std::cout<<"Succeed!"<<std::endl;
 }
@@ -41,8 +49,11 @@ void checkRangeFullVerbose(long long unsigned int begin, long long unsigned int 
 	}
 }
 
+
 int main(){
+	
 	try{
+		std::cout<<checkConjecture(345);
 		std::cout<<"checkConjectureVerbose test"<<std::endl;
 		checkConjectureVerbose(35);
 		std::cout<<"===========\ncheckRangeVerbose test"<<std::endl;
