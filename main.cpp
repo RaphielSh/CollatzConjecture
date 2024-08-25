08d0c338128e4391ac7011362017d4020c07abfc
RaphielSh <raphielsh@gmail.com>
2024-08-25 21:09:06 +0300

    number cache added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include<stdexcept>
 #include<unordered_set>
 
+std::unordered_set<long long unsigned int> num_cache;
+
 void print(const auto& set){
 	for(const auto& elem : set)std::cout<<elem<<' ';
 	std::cout<<std::endl;
@@ -12,9 +14,12 @@ bool checkConjecture(long long unsigned int num){
 	std::unordered_set<long long unsigned int> lst;
 	while(1!=num){
 		num = (num%2) ? (3*num)+1 : num/2;
-		if(lst.contains(num))return false;
+		if(lst.contains(num))return false;	//infinite loop prevention
 		lst.insert(num);
-    	}
+		if(num_cache.contains(num)) return true;	//cached numbers gonna return true immediately
+	}
+	num_cache.insert(num);
+	print(lst);
 	return true;
 }
 
@@ -38,7 +43,7 @@ bool checkRange(long long unsigned int begin, long long unsigned int end){
 
 void checkRangeVerbose(long long unsigned int begin, long long unsigned int end){
 	for(;begin!=end+1;++begin){
-		if(!checkConjecture(begin))std::cout<<"Check Failed! Failed number: "<< begin<<std::endl;
+		if(!checkConjecture(begin))std::cout<<"Check Failed! Failed number: "<<begin<<std::endl;
 	}
 	std::cout<<"Succeed!"<<std::endl;
 }
@@ -53,13 +58,15 @@ void checkRangeFullVerbose(long long unsigned int begin, long long unsigned int 
 int main(){
 	
 	try{
-		std::cout<<checkConjecture(345);
-		std::cout<<"checkConjectureVerbose test"<<std::endl;
-		checkConjectureVerbose(35);
-		std::cout<<"===========\ncheckRangeVerbose test"<<std::endl;
-		checkRangeVerbose(65, 145);
-		std::cout<<"===========\ncheckRangeFullVerbose test"<<std::endl;
-		checkRangeFullVerbose(362, 390);
+		std::cout<<"Number cache test: "<<checkConjecture(222)<<std::endl;
+		std::cout<<"==========================="<<std::endl;
+		std::cout<<checkConjecture(444)<<std::endl;
+		//		std::cout<<"checkConjectureVerbose test"<<std::endl;
+//		checkConjectureVerbose(35);
+//		std::cout<<"===========\ncheckRangeVerbose test"<<std::endl;
+//		checkRangeVerbose(65, 145);
+//		std::cout<<"===========\ncheckRangeFullVerbose test"<<std::endl;
+//		checkRangeFullVerbose(362, 390);
 	}
 	catch(const std::invalid_argument& e){std::cout<<e.what()<<std::endl;}
 	return 0;
